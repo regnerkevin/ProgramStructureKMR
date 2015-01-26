2052898626809d80eb560b4bd82c8c45a4e0a124
regnerkevin <krw10108@saintjoe.edu>
2015-01-26 11:12:48 -0600

    Updated comments.

--- ProgramStructureKMR.js
+++ ProgramStructureKMR.js
@@ -1,2 +1,8 @@
 for (var line = "#"; line.length < 8; line += "#")
 	console.log(line);
+	
+// Kevin Regner
+// It is out of chapter 2. The looping a triangle. 
+// Date made was 1/26th.
+// What this code does is make a triangle of #'s. For every single line, A single # is added to every additional line. 
+// 1 on the first and then 2 on the second and so on and so forth.
